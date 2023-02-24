b4fcafe6b3aabdc8c3e2ccb83b52af4ae57a054f
MzPoju <adepojuoopeye@gmail.com>
2023-02-24 15:09:39 -0800

    Come on

--- var.c
+++ var.c
@@ -11,7 +11,7 @@ int is_chain(info_t *info, char *buf, size_t *p)
 {
 	size_t j = *p;
 
-	if (buf[j] == '|' && buf[j + 1] == '|'')
+	if (buf[j] == '|' && buf[j + 1] == '|')
 	{
 		buf[j] = 0;
 		j++;
