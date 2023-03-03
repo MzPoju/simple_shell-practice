c3ff7410c5c3351221143fe7a7c9241224de5f4a
MzPoju <adepojuoopeye@gmail.com>
2023-03-03 01:29:18 -0800

    Atoi

--- _atoi.c
+++ _atoi.c
@@ -1,14 +1,15 @@
 #include "shell.h"
 
+
 /**
- * interactive - returns true if shellis interactive mode
+ * interactive - returns true if shell is interactive mode
  * @info: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
-	return (isatty(STDN_FILENO) && info->readfd <= 2);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
